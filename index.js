eca4865287f8e130e02c28a9975b498151e096f5
Aimar Anand <aimaranand02@gmail.com>
2022-02-01 11:21:21 +0800

    part 2 devops with docker&nodejs videos"

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("<h2>Hi Aimarw</h2>");
+  res.send("<h2>Hi Aimars</h2>");
 });
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
